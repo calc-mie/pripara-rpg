bb15b5ef6471c225bd105369a28f652e7b0bad49
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2017-12-23 19:08:30 +0900

    Add logic

--- main.c
+++ main.c
@@ -8,8 +8,23 @@ void sc_name(void);
 void sc_select(void);
 void sc_clear(void);
 
+void load_savefile(void);
+
 int main(void) {
-  puts("Welcome to PriPara RPG!!");
+  int menu;
+
+  menu == sc_title();
+
+  switch(menu) {
+  case 0:
+    sc_name();
+    sc_select();
+    break;
+  case 1:
+    load_savefile();
+  }
+
+  sc_clear();
 
   return 0;
 }
