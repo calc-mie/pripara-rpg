6ad73befc36a719a5df9f8234b60f07731444276
j417832 <417832@m.mie-u.ac.jp>
2017-12-23 19:36:45 +0900

    Implement load_judge

--- main.c
+++ main.c
@@ -7,9 +7,24 @@ void sc_title(void);
 void sc_name(void);
 void sc_select(void);
 void sc_clear(void);
+void load_savedata(void);
 
 int main(void) {
   puts("Welcome to PriPara RPG!!");
 
   return 0;
 }
+
+void load_savedata(void) {
+  FILE *fp;
+  
+  fp = fopen("save.dat", "r");
+  if (fp == NULL) {
+    puts("You are fired!");
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
