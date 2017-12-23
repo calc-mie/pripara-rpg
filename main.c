946e58020dbe097505205b78813bf0f94fcdb1c5
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2017-12-23 19:00:03 +0900

    Add stub & global

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int idol_id;
+char name[256];
+
+void sc_title(void);
+void sc_name(void);
+void sc_select(void);
+void sc_clear(void);
+
 int main(void) {
   puts("Welcome to PriPara RPG!!");
 
